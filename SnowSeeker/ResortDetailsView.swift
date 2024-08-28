8524c21ee92e6de80b65d4601b08193e711a52ec
oliver <oliverhu.oh@gmail.com>
2024-08-28 12:39:48 -0700

    project updated - new views

---
+++ SnowSeeker/ResortDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  ResortDetailsView.swift
+//  SnowSeeker
+//
+//  Created by Oliver Hu on 8/28/24.
+//
+
+import SwiftUI
+
+struct ResortDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResortDetailsView()
+}
---
+++ SnowSeeker/SkiDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  SkiDetailsView.swift
+//  SnowSeeker
+//
+//  Created by Oliver Hu on 8/28/24.
+//
+
+import SwiftUI
+
+struct SkiDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SkiDetailsView()
+}
