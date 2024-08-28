af8d605bce3cb2e36bc0568962fedb1049bb7d8d
oliver <oliverhu.oh@gmail.com>
2024-08-28 10:34:36 -0700

    project updated - added welcomeview

---
+++ SnowSeeker/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  SnowSeeker
+//
+//  Created by Oliver Hu on 8/28/24.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
