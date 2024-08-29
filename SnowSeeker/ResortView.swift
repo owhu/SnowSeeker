d32b3b1a754796f42dd974539a19fb85abc185b3
oliver <oliverhu.oh@gmail.com>
2024-08-29 09:32:15 -0700

    project updated

--- SnowSeeker/ResortView.swift
+++ SnowSeeker/ResortView.swift
@@ -8,11 +8,40 @@
 import SwiftUI
 
 struct ResortView: View {
+    let resort: Resort
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(alignment: .leading, spacing: 0) {
+                Image(decorative: resort.id)
+                    .resizable()
+                    .scaledToFit()
+
+                HStack {
+                    ResortDetailsView(resort: resort)
+                    SkiDetailsView(resort: resort)
+                }
+                .padding(.vertical)
+                .background(.primary.opacity(0.1))
+                
+                Group {
+                    Text(resort.description)
+                        .padding(.vertical)
+
+                    Text("Facilities")
+                        .font(.headline)
+
+                    Text(resort.facilities, format: .list(type: .and))
+                        .padding(.vertical)
+                }
+                .padding(.horizontal)
+            }
+        }
+        .navigationTitle("\(resort.name), \(resort.country)")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 #Preview {
-    ResortView()
+    ResortView(resort: .example)
 }
--- SnowSeeker/SkiDetailsView.swift
+++ SnowSeeker/SkiDetailsView.swift
@@ -8,11 +8,28 @@
 import SwiftUI
 
 struct SkiDetailsView: View {
+    let resort: Resort
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Group {
+            VStack {
+                Text("Elevation")
+                    .font(.caption.bold())
+                Text("\(resort.elevation)m")
+                    .font(.title3)
+            }
+
+            VStack {
+                Text("Snow")
+                    .font(.caption.bold())
+                Text("\(resort.snowDepth)cm")
+                    .font(.title3)
+            }
+        }
+        .frame(maxWidth: .infinity)
     }
 }
 
 #Preview {
-    SkiDetailsView()
+    SkiDetailsView(resort: .example)
 }
